f160934746ca6850db81dc18fb4ed893044902b3
DnvMaster <dnvcomp@yandex.ru>
2023-07-22 21:23:39 +0300

    Database insert data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
+use Auth;
 
 class CategoryController extends Controller
 {
@@ -19,5 +22,21 @@ class CategoryController extends Controller
             'category_name.required'=> 'Пожалуйста, введите название категории.',
             'category_name.max'=>'Название категории, должно быть, не менее 255 символов.',
         ]);
+
+        Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now(),
+        ]);
+
+        /*
+        $category = new Category;
+        $category->category_name = $request->category_name;
+        $category->user_id = Auth::user()->id;
+        $category->save();
+        */
+
+        return Redirect()->back()->with('success','Категория была успешно добавлена');
+
     }
 }
